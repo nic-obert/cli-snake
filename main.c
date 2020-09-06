ec0fd7f783e83f099d3a33bad1cb49e98bd08250
Nicholas Obert <nchlsuba@gmail.com>
2020-09-06 13:35:11 +0200

    configured vscode tasks

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ncurses.h>
+#include <unistd.h>
+#include <time.h>
+
+
+int main(int argc, char const *argv[])
+{
+    printf("hello world");
+    getchar();
+    return 0;
+}
+
